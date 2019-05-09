e9cd7cafde6fe64aa8d7f1c1ff22bc7369762955
Jabez123 <it.jabez16@gmail.com>
2019-05-09 11:43:46 +0800

    Add author info and question creation date in question item

--- app/Question.php
+++ app/Question.php
@@ -15,6 +15,14 @@ class Question extends Model
     public function setTitleAttribute($value) {
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = str_slug($value);
+    }
 
+    public function getUrlAttribute() {
+        return route("questions.show", $this->id);
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
     }
 }
