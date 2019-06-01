bea00fed17235c09a8cef37da6b45945fc63707b
Jabez123 <it.jabez16@gmail.com>
2019-06-01 17:17:15 +0800

    Implement authorization using Policy

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -20,43 +20,47 @@
                     @include('layouts._messages')
 
                     @foreach ($questions as $question)
-                        <div class="media">
-                            <div class="d-flex flex-column counters">
-                                <div class="vote">
-                                    <strong>{{ $question->votes }}</strong> {{ str_plural('vote', $question->votes) }}
-                                </div>
-                            <div class="status {{ $question->status }}">
-                                    <strong>{{ $question->answers }}</strong> {{ str_plural('answer', $question->answers) }}
-                                </div>
-                                <div class="view">
-                                    {{ $question->views . " " . str_plural('view', $question->views) }}
-                                </div>
+                    <div class="media">
+                        <div class="d-flex flex-column counters">
+                            <div class="vote">
+                                <strong>{{ $question->votes }}</strong> {{ str_plural('vote', $question->votes) }}
                             </div>
-                            <div class="media-body">
-                                <div class="d-flex align-items-center">
-                                    <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
-                                    <div class="ml-auto">
+                            <div class="status {{ $question->status }}">
+                                <strong>{{ $question->answers }}</strong> {{ str_plural('answer', $question->answers) }}
+                            </div>
+                            <div class="view">
+                                {{ $question->views . " " . str_plural('view', $question->views) }}
+                            </div>
+                        </div>
+                        <div class="media-body">
+                            <div class="d-flex align-items-center">
+                                <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
+                                <div class="ml-auto">
+                                    @can('update', $question)
                                     <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                    @endcan
+                                    @can('delete', $question)
                                     <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
                                         @method('DELETE')
                                         @csrf
                                         <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
                                     </form>
+                                    @endcan
                                 </div>
-                                </div>
-                                <p class="lead">
-                                    Asked by
+                            </div>
+                            <p class="lead">
+                                Asked by
                                 <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
                                 <small class="text-muted">{{ $question->created_date }}</small>
-                                </p>
-                                {{ str_limit($question->body, 250) }}
-                            </div>
+                            </p>
+                            {{ str_limit($question->body, 250) }}
                         </div>
-                        <hr>
+                    </div>
+                    <hr>
                     @endforeach
 
                     <div class="mx-auto">
-                       {{ $questions->links() }}
+                        {{ $questions->links() }}
                     </div>
                 </div>
             </div>
